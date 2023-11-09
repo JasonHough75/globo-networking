33a475cbb651772f2fa5480baf46d87209c5b5f7
jason.hough1@googlemail.com <jason.hough1@googlemail.com>
2023-11-09 16:37:37 +0000

    Add workspace tag and make variables required

--- variables.tf
+++ variables.tf
@@ -9,7 +9,7 @@ variable "region" {
 }
 
 variable "environment" {
-  type    = string
+  type = string
 
 }
 
@@ -33,6 +33,6 @@ variable "cidr_block" {
 variable "public_subnets" {
   type        = map(string)
   description = "(Optional) Map of public subnets to create with CIDR blocks. Key will be used as subnet name with prefix. Default: {subnet-1 ="
-  
+
 }
 
